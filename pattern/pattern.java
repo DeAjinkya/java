68ddfb3d5f20ca60337f19e51c82d5aa00b55e36
Ajinkya Deshmukh <115269019+DeAjinkya@users.noreply.github.com>
2025-09-25 22:35:32 +0530

    Q1

---
+++ pattern/pattern.java
@@ -0,0 +1,21 @@
+class pattern{
+    public static void main(String[] args){
+        //outer loop = rows
+        for(int i = 0; i<4; i++){
+            System.out.print("*");
+            // inner loop = columns
+            for(int j = 1;j<5;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+}
+
+/**
+ *          *****
+            *****
+            *****
+            *****
+ */
